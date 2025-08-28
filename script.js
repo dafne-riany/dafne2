2f0cce4da3d677c3cc7612918869535439458d19
dafne-riany <00001219726321sp@al.educacao.sp.gov.br>
2025-08-28 18:15:26 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    ScrollReveal().reveal('#home', { delay: 200 });
+    ScrollReveal().reveal('#sobre', { delay: 400 });
+    ScrollReveal().reveal('#contato', { delay: 600 });
+});
